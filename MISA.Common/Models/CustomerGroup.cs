a7435290ff3048a76c582421ed7b5a2ee3513bf2
Trinh67 <56555813+Trinh67@users.noreply.github.com>
2021-02-06 14:17:01 +0700

    niter

---
+++ MISA.Common/Models/CustomerGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.Common.Models
+{
+    public class CustomerGroup
+    {
+        public Guid CustomerGroupId { get; set; }
+    }
+}
---
+++ MISA.Common/Models/ErroMsg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.Common.Models
+{
+    public class ErroMsg
+    {
+        public string DevMsg { get; set; }
+        public string UserMsg { get; set; }
+        public string ErrorCode { get; set; }
+        public string MoreInfo { get; set; }
+        public string TraceId { get; set; }
+    }
+}
